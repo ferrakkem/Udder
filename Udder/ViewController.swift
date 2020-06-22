c8533815aeb0144cb284c57db1f60760367fb082
Ferrakkem Bhuiyan <ferrakkem@gmail.com>
2020-06-22 17:44:15 -0400

    Initial Commit

---
+++ Udder/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Udder
+//
+//  Created by Ferrakkem Bhuiyan on 2020-06-22.
+//  Copyright © 2020 Ferrakkem Bhuiyan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
